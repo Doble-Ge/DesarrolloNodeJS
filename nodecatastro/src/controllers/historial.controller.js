b3cee90ed9feefc3f08115d4fd50b99117e3c96b
Doble-Ge <ginogutierrez95@gmail.com>
2021-10-02 10:14:35 -0300

    controlador de historial completo - BD actualizada tablas mensura

---
+++ nodecatastro/src/controllers/historial.controller.js
@@ -0,0 +1,125 @@
+import { text } from 'express';
+import Historial from '../models/historial';
+
+export async function getHistorial(req,res) {
+    try{
+        const historial = await Historial.findAll();
+        res.json({
+            data: historial
+        });
+    } catch (e) {
+        console.log(e);
+    }
+   
+}
+
+export async function crearHistorial(req, res) {
+    const {estado, fechahora, tramite_id, usuario_id} = req.body;
+    try {
+
+        let nuevoHistorial = await Historial.create({
+            estado,
+            fechahora,
+            tramite_id,
+            usuario_id
+        }, {
+            fields:['estado', 'fechahora', 'tramite_id', 'usuario_id']
+        });
+        if (nuevoHistorial) {
+            return res.json({
+                message: 'Registrado',
+                data: nuevoHistorial
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            message: 'algo va mal',
+            data: {}
+        });
+    }
+    
+}
+
+export async function getOneHistorial(req,res){
+    try {
+        const {id} = req.params;
+    const historial = await Historial.findOne({
+        where: {
+            id: id
+        }
+    });
+    res.json({
+        data: historial
+    });
+    }catch (e){
+        console.log(e);
+    }
+}
+
+/*export async function getOneUsuarioCuil(req,res){
+    try {
+        const {cuil} = req.params;
+    const usuario = await UsuarioA.findOne({
+        where: {
+            cuil: cuil
+        }
+    });
+    res.json({
+        data: usuario
+    });
+    }catch (e){
+        console.log(e);
+    }
+}*/
+
+
+export async function deleteHistorial(req,res){
+    try {
+        const {id} = req.params;
+    const deleteRowCount = await Historial.destroy({
+        where: {
+            id: id
+        }
+    });
+    res.json({
+        message: "Eliminado",
+        count: deleteRowCount
+    });
+    }catch (e){
+        console.log(e);
+    }
+}
+
+export async function updateHistorial(req,res){
+    try{
+        const {id} = req.params;
+    const {estado, fechahora, tramite_id, usuario_id} = req.body;
+    
+    const historial = await Historial.findAll({
+        attributes: ['estado', 'fechahora', 'tramite_id', 'usuario_id'],
+        where: {
+            id: id
+        }
+    });
+
+    if(historial.length > 0){
+        historial.forEach(async historial => {
+            await historial.update({
+                estado,
+                fechahora,
+                tramite_id,
+                usuario_id
+            })
+            
+        });
+    }
+
+    return res.json({
+        message: "Historial actualizado con éxito",
+        data: historial
+    });
+    }catch (e){
+        console.log(e);
+    }
+}
