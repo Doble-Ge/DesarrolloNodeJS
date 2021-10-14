d4ebeaf97ef5c0173959a98bda6cf00bba61acaf
mdfuentes <84162357+mdfuentes@users.noreply.github.com>
2021-10-14 19:33:31 -0300

    Recuperacion de clave terminada

--- MaquetaPrueba/src/app/recuperar-clave/recuperar-clave.component.ts
+++ MaquetaPrueba/src/app/recuperar-clave/recuperar-clave.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Correo } from '../models/correoClave';
 import { CorreoClaveService } from '../services/correo-clave.service';
+import { UsuarioService } from '../services/usuario.service';
 
 @Component({
   selector: 'app-recuperar-clave',
@@ -9,24 +10,52 @@ import { CorreoClaveService } from '../services/correo-clave.service';
 })
 export class RecuperarClaveComponent implements OnInit {
   mail: Correo = new Correo()
-  constructor(private mailInyectado: CorreoClaveService) { }
+  cuil: string
+  usuarioDesdeBase
+  clave: string
+  constructor(private mailInyectado: CorreoClaveService, private usuarioInyectado: UsuarioService) { }
 
   ngOnInit(): void {
+
   }
 
   enviarCorreo(){
     this.generarClave()
+    this.getUsuario()
     this.mailInyectado.postMail(this.mail).subscribe(
       res => {
         console.log(res)
+        this.updateClave()
       },
       err => console.error(err)
     )
   }
 
   generarClave(){
-    const clave = '1231245'
-    this.mail.mensaje = `Tu nueva clave de recuperacion es: ${clave}`
+    
+    this.clave = (Math.floor(Math.random() * (999999 - 100000)) + 100000).toString()
+    this.mail.mensaje = `Tu nueva clave de recuperacion es: ${this.clave}`
+  }
+
+  getUsuario(){
+    this.usuarioInyectado.getUsuario(this.cuil).subscribe(
+      res => {
+        this.usuarioDesdeBase = Object.values(res)
+        console.log(this.usuarioDesdeBase)
+      },
+      err => console.error(err)
+    )
+  }
+
+  updateClave(){
+    this.usuarioDesdeBase[0].pass = this.clave
+    this.usuarioDesdeBase[0].primerLogin = true
+    this.usuarioInyectado.updateContraseña(this.cuil, this.usuarioDesdeBase[0]).subscribe(
+      res => {
+        alert("Nueva contraseña enviada al email")
+      },
+      err => console.error(err)
+    )
   }
 
 }
